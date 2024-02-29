4fdaaaf2efe7b01027d9d63f096e7a5370938e1a
groovinflannel <groovinflannel@gmail.com>
2024-02-29 14:00:29 -0500

    Questions activity created; OnClickListener added to start button to move to Questions activity

---
+++ app/src/main/java/com/example/goinggoinggonequizapp/ui/QuestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.goinggoinggonequizapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.goinggoinggonequizapp.R
+
+class QuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questions)
+    }
+}
